d5ba5d0779ad7d731653a22c77a2e51044c14737
Robson Junior <111658874+RobCoffe@users.noreply.github.com>
2022-11-07 15:09:23 -0300

    Ajuste para aplicação de arrays para os carros.

--- carro.js
+++ carro.js
@@ -1,30 +1,31 @@
 //Codigos dos carros
+let yCarros = [40,96,150];
+let velocidadeCarros = [3.5, 5, 6];
+
 
 //Variaveis do Carro 1
 let xCarro1 = 600;
-let yCarro1 = 40;
-let velocidadeCarro1 = 3.5;
+
 
 //Variaveis do Carro 2
 let xCarro2 = 600;
-let yCarro2 = 96;
-let velocidadeCarro2 = 5;
+
 
 //Variaveis do Carro 3
 let xCarro3 = 600;
-let yCarro3 = 146;
-let velocidadeCarro3 = 6;
+
 
 function mostraCarro() {
-  image(imagemCarro, xCarro1, yCarro1, 40, 30);
-  image(imagemCarro2, xCarro2, yCarro2, 40, 30);
-  image(imagemCarro3, xCarro3, yCarro3, 40, 30);
+  image(imagemCarro, xCarro1, yCarros[0], 40, 30);
+  image(imagemCarro2, xCarro2, yCarros[1], 40, 30);
+  image(imagemCarro3, xCarro3, yCarros[2],40, 30);
+  
 }
 
 function movimentaCarro() {
-  xCarro1 -= velocidadeCarro1;
-  xCarro2 -= velocidadeCarro2;
-  xCarro3 -= velocidadeCarro3;
+  xCarro1 -= velocidadeCarros[0];
+  xCarro2 -= velocidadeCarros[1];
+  xCarro3 -= velocidadeCarros[2];
 }
 
 function voltaPosicaoInicial(){
